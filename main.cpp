731fd07dfc0811276819640e2297a2790c47dcf6
Antonio <jr_acn@yahoo.com.br>
2016-05-03 23:02:40 -0300

    criado método

--- main.cpp
+++ main.cpp
@@ -3,15 +3,8 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int troca(char a[4], char b[4]){
 		
-	char a[4] = {};
-	char b[4] = {};
-	cout<< "Digite o valor de A\n";
-	cin>>a;
-	cout<< "Digite o valor de B\n";
-	cin>>b;
-	
 	int igual = 1;
 	
 	int i = 0;
@@ -22,6 +15,19 @@ int main(int argc, char** argv) {
 		}
 		j--;
 	}
+	return igual;
+}
+
+int main(int argc, char** argv) {
+		
+	char a[4] = {};
+	char b[4] = {};
+	cout<< "Digite o valor de A\n";
+	cin>>a;
+	cout<< "Digite o valor de B\n";
+	cin>>b;
+
+	int igual = troca(a,b);
 	
 	if(igual == 0){
 		 cout << "Sao diferentes\n" ;
@@ -31,5 +37,6 @@ int main(int argc, char** argv) {
 	}
  
   system("pause");
-	return igual;
+return igual;
 }
+
